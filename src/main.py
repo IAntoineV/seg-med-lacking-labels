bc9e075fba3666796852f9497f444629462725e3
Kerrian Le Caillec <kerrian.lecaillec@student-cs.fr>
2025-02-13 10:40:13 +0100

    updated main loop

--- src/main.py
+++ src/main.py
@@ -1,37 +1,86 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import hydra
-
-from tqdm import tqdm
-from omegaconf import DictConfig
-from dataset import get_data
 from torch.utils.data import DataLoader
 from torchvision import models
+from tqdm import tqdm
+from dataset import get_data
 from utils import to_one_hot
 from metrics import dice
 import importlib
 
+# Import SAM and related models from Hugging Face Transformers
+from transformers import (
+    SamModel,
+    SamConfig,
+    SamVisionConfig,
+    SamMaskDecoderConfig,
+    SamPromptEncoderConfig,
+    SamImageProcessor,
+    DetrImageProcessor,
+    DetrForObjectDetection,
+
+)
+
 num_classes = 55
 batch_size = 16
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model_configs = {
+    "deeplabv3_mobilenet_v3_large": "torchvision.models.segmentation.deeplabv3_mobilenet_v3_large",
+    "deeplabv3_resnet50": "torchvision.models.segmentation.deeplabv3_resnet50",
+    "sam_transformer": "transformers.SamModel",
+}
 
-@hydra.main(config_path="./config", config_name="models")
-def main(cfg: DictConfig):
+def main(model_name="deeplabv3_mobilenet_v3_large"):
     data_train, data_test = get_data(test_size=0.05)
 
-    dataloader = DataLoader(data_train, batch_size=batch_size, shuffle=True, num_workers=2)
+    dataloader = DataLoader(
+        data_train, batch_size=batch_size, shuffle=True, num_workers=2
+    )
     data_test_loader = DataLoader(data_test, batch_size=batch_size, num_workers=2)
 
-    module_name, class_name = cfg.model.class_path.rsplit(".", 1)
-    model_class = getattr(importlib.import_module(module_name), class_name)
-    model = model_class(pretrained=cfg.model.pretrained, num_classes=num_classes).to(device)
+    if model_name == "sam_transformer":
+
+        vision_config = SamVisionConfig(
+            num_hidden_layers=1,
+            num_attention_heads=4,
+            hidden_size=128,
+        )
+        mask_decoder_config = SamMaskDecoderConfig(
+            num_hidden_layers=1,
+            num_attention_heads=4,
+        )
+
+        prompt_encoder_config = SamPromptEncoderConfig()
+        config = SamConfig(
+            vision_config=vision_config,
+            mask_decoder_config=mask_decoder_config,
+            prompt_encoder_config=prompt_encoder_config,
+        )
+
+        model = SamModel(config).to(device)
+        processor = SamImageProcessor(
+            do_rescale=False,
+            do_resize=False,
+        )
+        
+        cfg = model_configs["deeplabv3_mobilenet_v3_large"]
+        module_name, class_name = cfg.rsplit(".", 1)
+        dlv3_model_class = getattr(importlib.import_module(module_name), class_name)
+        dlv3_model = dlv3_model_class(pretrained=False, num_classes=num_classes).to(device)
+    else:
+        cfg = model_configs[model_name]
+        module_name, class_name = cfg.rsplit(".", 1)
+        model_class = getattr(importlib.import_module(module_name), class_name)
+        model = model_class(pretrained=False, num_classes=num_classes).to(device)
+        processor = None
+
     num_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f"Total trainable parameters: {num_trainable_params:,}")
 
     def dice_criterion(outputs, labels):
         labels_hot = to_one_hot(labels, num_classes).to(device)
-        dice_score = dice(labels_hot, torch.nn.functional.softmax(outputs['out'], dim=1))
+        dice_score = dice(labels_hot, torch.nn.functional.softmax(outputs, dim=1))
         present_labels = torch.unique(labels)
         mask = torch.zeros(num_classes, device=device)
         mask[present_labels] = 1
@@ -51,7 +100,14 @@ def main(cfg: DictConfig):
             images = images.to(device)  # (B,1 , H, W)
             labels = labels.to(device)  # (B, H, W) with class indices
             optimizer.zero_grad()
-            outputs = model(images)  # (B, num_classes, H, W)
+            if model_name == "sam_transformer":
+                boxes = dlv3_model(images)["out"]
+                print(boxes)
+                inputs = processor(images, segmentation_maps=boxes, return_tensors="pt").to(device)
+                outputs = model(**inputs)["pred_masks"]
+
+            else:
+                model(images)["out"]
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()
@@ -64,11 +120,19 @@ def main(cfg: DictConfig):
         with torch.no_grad():
             dice_cum = 0
             for x_test, y_test in data_test_loader:
-                output = model(x_test.to(device))
+                if model_name == "sam_transformer":
+                    inputs = processor(x_test.to(device), return_tensors="pt").to(
+                        device
+                    )
+                    output = model(**inputs)["pred_masks"]
+                    
+                else:
+                    output = model(x_test.to(device))["out"]
+
                 dice_score = dice_criterion(output, y_test.to(device))
                 dice_cum += (1 - dice_score.item()) * x_test.size(0)
             print("valid dice ", dice_cum / len(data_test))
     print("Training complete.")
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main(model_name="deeplabv3_resnet50")
