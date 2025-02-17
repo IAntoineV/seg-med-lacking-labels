0244db4ed33a08a82d579f0e98264de7b34c45fe
IAntoineV <antoinevialle.ia@gmail.com>
2025-02-17 22:26:37 +0100

    [New box losses]

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import torch.nn.functional as F
-
+import torch
 def to_one_hot(labels, nb_class):
     """
     Converts a tensor of labels with shape (n, w, h) into a one-hot encoded tensor of shape (n, nb_class, w, h).
@@ -16,4 +16,24 @@ def to_one_hot(labels, nb_class):
     one_hot = F.one_hot(labels, num_classes=nb_class) # (n, w, h, nb_class)
     one_hot = one_hot.permute(0, 3, 1, 2) # (n, nb_class, w, h)
     return one_hot
+
+
+def box_criterion(box_outputs, box_true, delta_length=20):
+    return (((box_outputs - box_true)/delta_length) ** 2).mean()
+
+
+def lax_box_criterion(box_outputs, box_true, delta_length=20):
+    """ box : (xmin, ymin, xmax, ymax)
+
+    Lax loss wants to make sure the predicted boxes contains the true boxes. It should be use with classic box criterion
+    as a type of regularizer that optimize the inference dice"""
+    delta_box = box_outputs - box_true # (b, num_classes, 4)
+    condition_swapper = torch.Tensor([-1,-1,1,1.]) # we want xmin and ymin to be under true box and xmax ymax to be above.
+    condition_swapper = condition_swapper.to(box_outputs.device).view(1,1,-1)
+    loss = (((F.relu(condition_swapper * delta_box))/delta_length)**2).mean()
+    return loss
+
+
+
+
 
